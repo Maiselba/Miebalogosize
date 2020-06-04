c12ff7992fb4b3c43b57af6c79732023300a9096
Maiselba <66247904+Maiselba@users.noreply.github.com>
2020-06-04 09:55:50 +0200

    Initial commit

---
+++ src/Miebalogosize.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Miebalogosize;
+
+use Shopware\Core\Framework\Plugin;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class Miebalogosize extends Plugin
+{
+
+}
